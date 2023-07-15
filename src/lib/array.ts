d3f8de406d0047eafe852345e286a56e1d4b77a3
Nanase <nanase@users.noreply.github.com>
2023-07-16 07:33:24 +0900

    Add array.ts and unittest

---
+++ src/lib/array.ts
@@ -0,0 +1,28 @@
+export function findBy<K extends PropertyKey, T extends Record<K, any>, ValueType>(
+  key: K,
+  value: ValueType,
+  array: Array<T>,
+): T | null {
+  for (const element of array) {
+    if (typeof element[key] === 'undefined') continue;
+    if (element[key] === value) return element;
+  }
+  return null;
+}
+
+export function mergeArrayBy<K extends PropertyKey, T1 extends Record<K, any>, T2 extends Record<K, any>>(
+  key: K,
+  array1: T1[],
+  array2: T2[],
+): Array<T1 & T2> {
+  return array1
+    .map((element) => {
+      const obj1 = findBy(key, element[key], array1);
+      const obj2 = findBy(key, element[key], array2);
+
+      if (obj1 == null || obj2 == null) return null;
+
+      return { ...obj1, ...obj2 };
+    })
+    .filter((e): e is NonNullable<typeof e> => e !== null);
+}
