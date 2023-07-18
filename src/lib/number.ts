ddcbbe47e12ec911ec2b857fbd5fc3c6e1ee0e04
Nanase <nanase@users.noreply.github.com>
2023-07-18 18:26:28 +0900

    Add unittest for withCommas

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -1,3 +1,4 @@
-export function withCommas(x?: number): string | undefined {
-  return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+// from: https://stackoverflow.com/a/2901298
+export function withCommas(x: number): string {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
