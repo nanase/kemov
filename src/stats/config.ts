35edb5fa3132b270be365d5a6298ce9c7e80aa24
Nanase <nanase@users.noreply.github.com>
2023-07-19 21:08:44 +0900

    Change channel.json uri

--- src/stats/config.ts
+++ src/stats/config.ts
@@ -1,2 +1,2 @@
-export const channelsUri = 'https://raw.githubusercontent.com/nanase/asset/main/kemov/channel.json';
+export const channelsUri = 'https://raw.githubusercontent.com/nanase/asset/main/kemov/channels.json';
 export const statsUri = 'https://s3.ap-northeast-1.amazonaws.com/nanase.asset/kemov/stats.json';
