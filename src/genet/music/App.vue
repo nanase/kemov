4059dd62e1069a097374b954074af1c44b4c33c0
Nanase <nanase@users.noreply.github.com>
2023-07-31 05:08:48 +0900

    Add infinite-loading to genet/music/App.vue

--- src/genet/music/App.vue
+++ src/genet/music/App.vue
@@ -1,15 +1,54 @@
 <script setup lang="ts">
 import StreamingItem from '@/components/genet/StreamingItem.vue';
+import { ref } from 'vue';
+import InfiniteLoading from 'v3-infinite-loading';
+import type { StateHandler } from 'v3-infinite-loading/lib/types';
 import type { Streaming } from '@/types/genet';
 
 import { load as loadYaml } from 'js-yaml';
 import streamingYaml from './streaming.yml?raw';
 
 const streamings = loadYaml(streamingYaml) as Streaming[];
+const displayedStreamings = ref<Streaming[]>([]);
+const ItemsPerLoaded = 100;
+
+const load = async (state: StateHandler) => {
+  if (displayedStreamings.value.length >= streamings.length) {
+    state.complete();
+  } else {
+    const newItems = streamings.slice(
+      displayedStreamings.value.length,
+      displayedStreamings.value.length + ItemsPerLoaded,
+    );
+    displayedStreamings.value.push(...newItems);
+    state.loading();
+  }
+};
 </script>
 
 <template>
   <div class="streaming-list">
-    <StreamingItem v-for="streaming in streamings" :key="streaming.video.id" :data="streaming" />
+    <TransitionGroup name="streaming-item">
+      <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
+    </TransitionGroup>
   </div>
+  <InfiniteLoading @infinite="load">
+    <template #complete>
+      <span></span>
+    </template>
+  </InfiniteLoading>
 </template>
+
+<style lang="scss">
+.streaming-item {
+  &-enter-active,
+  &-leave-active {
+    transition: opacity 0.3s ease;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+  }
+}
+</style>
