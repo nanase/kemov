9ec6113ada28156adbb732e129ecaadd534fab1f
Nanase <nanase@users.noreply.github.com>
2023-08-21 17:30:30 +0900

    Add keyword search

--- src/genet/music/App.vue
+++ src/genet/music/App.vue
@@ -9,15 +9,18 @@ import StreamingItem from '@/components/genet/StreamingItem.vue';
 
 import streamingYaml from './streaming.yml?raw';
 
-const streamings = loadYaml(streamingYaml) as Streaming[];
+const rawStreamings = loadYaml(streamingYaml) as Streaming[];
+
+const streamings = ref<Streaming[]>([...rawStreamings]);
 const displayedStreamings = ref<Streaming[]>([]);
-const ItemsPerLoaded = 100;
+const ItemsPerLoaded = 10;
+const infiniteId = ref<number>(0);
 
 const load = async (state: StateHandler) => {
-  if (displayedStreamings.value.length >= streamings.length) {
+  if (displayedStreamings.value.length >= streamings.value.length) {
     state.complete();
   } else {
-    const newItems = streamings.slice(
+    const newItems = streamings.value.slice(
       displayedStreamings.value.length,
       displayedStreamings.value.length + ItemsPerLoaded,
     );
@@ -25,19 +28,50 @@ const load = async (state: StateHandler) => {
     state.loading();
   }
 };
+
+function updateValue(e: Event) {
+  if (!(e.target instanceof HTMLInputElement)) return;
+
+  const query = e.target.value;
+
+  if (query.length === 0) {
+    streamings.value = [...rawStreamings];
+  } else {
+    streamings.value = rawStreamings.filter((s) => s.name.includes(query));
+  }
+
+  displayedStreamings.value = [];
+  infiniteId.value = infiniteId.value + 1; // force-reset the InfiniteLoading
+}
 </script>
 
 <template>
-  <div class="streaming-list">
-    <TransitionGroup name="streaming-item">
-      <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
-    </TransitionGroup>
+  <div class="search-box">
+    <div class="keyword">
+      <label class="label" for="keyword">キーワード検索</label>
+      <input class="input" type="search" id="keyword" placeholder="動画名、曲名、作曲者名など…" @change="updateValue" />
+    </div>
+    <div class="result-count" v-show="streamings != null">
+      <span v-if="streamings.length === rawStreamings.length">
+        {{ `${rawStreamings.length} 件の配信または動画` }}
+      </span>
+      <span v-else>
+        {{ `${rawStreamings.length} 件の配信または動画から ${streamings.length} 件 見つかりました` }}
+      </span>
+    </div>
   </div>
-  <InfiniteLoading @infinite="load">
-    <template #complete>
-      <span></span>
-    </template>
-  </InfiniteLoading>
+  <div class="content">
+    <div class="streaming-list">
+      <TransitionGroup name="streaming-item">
+        <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
+      </TransitionGroup>
+    </div>
+    <InfiniteLoading :identifier="infiniteId" @infinite="load">
+      <template #complete>
+        <span></span>
+      </template>
+    </InfiniteLoading>
+  </div>
 </template>
 
 <style lang="scss">
@@ -78,5 +112,22 @@ const load = async (state: StateHandler) => {
 
 .content {
   background-color: white;
+}
+
+.search-box {
+  text-align: center;
+
+  .input {
+    padding: 10px;
+    margin-left: 20px;
+    width: 50%;
+    background-color: #d3d6da;
+    border: none;
+    border-radius: 5px;
+  }
+
+  .result-count {
+    margin-top: 1em;
+  }
 }
 </style>
