24871afe5b7104ab0d2875240e1ee5ced0db108c
Nanase <nanase@users.noreply.github.com>
2023-08-23 17:49:20 +0900

    Add class music-app in MusicApp.vue

--- src/genet/music/MusicApp.vue
+++ src/genet/music/MusicApp.vue
@@ -50,131 +50,135 @@ function updateSearchQuery(e: Event) {
 </script>
 
 <template>
-  <div class="search-box">
-    <div class="keyword">
-      <label class="label" for="keyword">キーワード検索</label>
-      <input
-        class="input"
-        type="search"
-        id="keyword"
-        placeholder="動画名、曲名、作曲者名など…"
-        @change="updateSearchQuery"
-      />
+  <div class="music-app">
+    <div class="search-box">
+      <div class="keyword">
+        <label class="label" for="keyword">キーワード検索</label>
+        <input
+          class="input"
+          type="search"
+          id="keyword"
+          placeholder="動画名、曲名、作曲者名など…"
+          @change="updateSearchQuery"
+        />
+      </div>
+      <div class="result-count" v-show="streamings != null">
+        <span v-if="streamings.length === rawStreamings.length">
+          {{ `${rawStreamings.length} 件の配信` }}
+        </span>
+        <span v-else>
+          {{ `${rawStreamings.length} 件の配信から ${streamings.length} 件 見つかりました` }}
+        </span>
+      </div>
     </div>
-    <div class="result-count" v-show="streamings != null">
-      <span v-if="streamings.length === rawStreamings.length">
-        {{ `${rawStreamings.length} 件の配信` }}
-      </span>
-      <span v-else>
-        {{ `${rawStreamings.length} 件の配信から ${streamings.length} 件 見つかりました` }}
-      </span>
+    <div class="content">
+      <div class="streaming-list">
+        <TransitionGroup name="streaming-item">
+          <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
+        </TransitionGroup>
+      </div>
+      <InfiniteLoading :identifier="infiniteId" @infinite="load">
+        <template #complete>
+          <div
+            class="finish-score"
+            style="background-image: url('/kemov/genet/score0.svg')"
+            v-if="finishScore === 0"
+          ></div>
+          <div
+            class="finish-score"
+            style="background-image: url('/kemov/genet/score1.svg')"
+            v-if="finishScore === 1"
+          ></div>
+          <div
+            class="finish-score"
+            style="background-image: url('/kemov/genet/score2.svg')"
+            v-if="finishScore === 2"
+          ></div>
+          <div
+            class="finish-score"
+            style="background-image: url('/kemov/genet/score3.svg')"
+            v-if="finishScore === 3"
+          ></div>
+          <div
+            class="finish-score"
+            style="background-image: url('/kemov/genet/score4.svg')"
+            v-if="finishScore === 4"
+          ></div>
+        </template>
+      </InfiniteLoading>
     </div>
-  </div>
-  <div class="content">
-    <div class="streaming-list">
-      <TransitionGroup name="streaming-item">
-        <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
-      </TransitionGroup>
-    </div>
-    <InfiniteLoading :identifier="infiniteId" @infinite="load">
-      <template #complete>
-        <div
-          class="finish-score"
-          style="background-image: url('/kemov/genet/score0.svg')"
-          v-if="finishScore === 0"
-        ></div>
-        <div
-          class="finish-score"
-          style="background-image: url('/kemov/genet/score1.svg')"
-          v-if="finishScore === 1"
-        ></div>
-        <div
-          class="finish-score"
-          style="background-image: url('/kemov/genet/score2.svg')"
-          v-if="finishScore === 2"
-        ></div>
-        <div
-          class="finish-score"
-          style="background-image: url('/kemov/genet/score3.svg')"
-          v-if="finishScore === 3"
-        ></div>
-        <div
-          class="finish-score"
-          style="background-image: url('/kemov/genet/score4.svg')"
-          v-if="finishScore === 4"
-        ></div>
-      </template>
-    </InfiniteLoading>
   </div>
 </template>
 
 <style lang="scss">
 @use '@/style/media';
 
-.streaming-item {
-  &-enter-active,
-  &-leave-active {
-    transition: opacity 0.3s ease;
+.music-app {
+  .streaming-item {
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 0.3s ease;
+    }
+
+    &-enter-from,
+    &-leave-to {
+      opacity: 0;
+    }
   }
 
-  &-enter-from,
-  &-leave-to {
-    opacity: 0;
-  }
-}
-
-.content,
-.search-box {
-  width: 85%;
-  margin: 20px auto;
-  padding: 15px 35px;
-  border-radius: 10px;
-
-  @include media.size(lg) {
-    width: 815px;
-  }
-
-  @include media.size(md) {
+  .content,
+  .search-box {
     width: 85%;
+    margin: 20px auto;
+    padding: 15px 35px;
+    border-radius: 10px;
+
+    @include media.size(lg) {
+      width: 815px;
+    }
+
+    @include media.size(md) {
+      width: 85%;
+    }
+
+    @include media.size(sm) {
+      width: 100%;
+      padding: 15px 0;
+    }
   }
 
-  @include media.size(sm) {
+  .content {
+    background-color: white;
+  }
+
+  .search-box {
+    text-align: center;
+
+    .input {
+      padding: 10px;
+      margin-left: 20px;
+      width: 50%;
+      background-color: #d3d6da;
+      border: none;
+      border-radius: 5px;
+    }
+
+    .result-count {
+      margin-top: 1em;
+    }
+  }
+
+  .finish-score {
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: contain;
     width: 100%;
-    padding: 15px 0;
-  }
-}
+    height: 80px;
+    margin: 80px 0 50px;
 
-.content {
-  background-color: white;
-}
-
-.search-box {
-  text-align: center;
-
-  .input {
-    padding: 10px;
-    margin-left: 20px;
-    width: 50%;
-    background-color: #d3d6da;
-    border: none;
-    border-radius: 5px;
-  }
-
-  .result-count {
-    margin-top: 1em;
-  }
-}
-
-.finish-score {
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: contain;
-  width: 100%;
-  height: 80px;
-  margin: 80px 0 50px;
-
-  @include media.size(md) {
-    height: 66px;
+    @include media.size(md) {
+      height: 66px;
+    }
   }
 }
 </style>
