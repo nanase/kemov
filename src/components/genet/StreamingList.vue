8d31af390d8d13f1badc7fa2154382c8d3b822e2
Nanase <nanase@users.noreply.github.com>
2023-08-27 18:59:49 +0900

    Add StreamingList.vue

---
+++ src/components/genet/StreamingList.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import InfiniteLoading from 'v3-infinite-loading';
+import type { StateHandler } from 'v3-infinite-loading/lib/types';
+import { load as loadYaml } from 'js-yaml';
+import { StreamingSearch } from './search';
+import axios from 'axios';
+
+import type { Streaming } from '@/types/genet';
+import StreamingItem from '@/components/genet/StreamingItem.vue';
+
+const { filterQuery } = defineProps<{
+  filterQuery: string;
+}>();
+
+const emit = defineEmits<{
+  updateFilter: [total: number, matched: number];
+}>();
+
+const rawStreamings = loadYaml(
+  await (async function () {
+    if (import.meta.env.VITE_GENET_MUSIC_LIST_SUB_URL) {
+      try {
+        const responce = await axios.get<string>(import.meta.env.VITE_GENET_MUSIC_LIST_SUB_URL);
+        console.info('This is development build. Fetched from sub url');
+
+        return responce.data;
+      } catch (error) {
+        if (import.meta.env.DEV) {
+          console.info(
+            `This is development build. Try to fetch from sub url, but fetching was failed caused by ${error}`,
+          );
+        }
+      }
+    }
+
+    return (await axios.get<string>(import.meta.env.VITE_GENET_MUSIC_LIST_URL)).data;
+  })(),
+) as Streaming[];
+
+const streamings = ref<Streaming[]>([...rawStreamings]);
+const displayedStreamings = ref<Streaming[]>([]);
+const ItemsPerLoaded = 10;
+const infiniteId = ref<number>(0);
+const finishScore = ref<number>(0);
+const streamingSearch = new StreamingSearch();
+
+emit('updateFilter', rawStreamings.length, rawStreamings.length);
+
+const load = async (state: StateHandler) => {
+  if (displayedStreamings.value.length >= streamings.value.length) {
+    finishScore.value = Math.floor(Math.random() * 5);
+    state.complete();
+  } else {
+    const newItems = streamings.value.slice(
+      displayedStreamings.value.length,
+      displayedStreamings.value.length + ItemsPerLoaded,
+    );
+    displayedStreamings.value.push(...newItems);
+    state.loading();
+  }
+};
+
+watch(
+  () => filterQuery,
+  () => {
+    if (filterQuery.length === 0) {
+      streamings.value = [...rawStreamings];
+    } else {
+      streamings.value = streamingSearch.search(rawStreamings, filterQuery);
+    }
+
+    emit('updateFilter', rawStreamings.length, streamings.value.length);
+
+    displayedStreamings.value = [];
+    infiniteId.value = infiniteId.value + 1; // force-reset the InfiniteLoading
+  },
+);
+</script>
+
+<template>
+  <div class="streaming-list">
+    <TransitionGroup name="streaming-item">
+      <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
+    </TransitionGroup>
+  </div>
+  <InfiniteLoading :identifier="infiniteId" @infinite="load">
+    <template #complete>
+      <div class="finish-score" style="background-image: url('/kemov/genet/score0.svg')" v-if="finishScore === 0"></div>
+      <div class="finish-score" style="background-image: url('/kemov/genet/score1.svg')" v-if="finishScore === 1"></div>
+      <div class="finish-score" style="background-image: url('/kemov/genet/score2.svg')" v-if="finishScore === 2"></div>
+      <div class="finish-score" style="background-image: url('/kemov/genet/score3.svg')" v-if="finishScore === 3"></div>
+      <div class="finish-score" style="background-image: url('/kemov/genet/score4.svg')" v-if="finishScore === 4"></div>
+      <div class="horizon"></div>
+      <div class="note">
+        <ul>
+          <li>
+            このサイトは非公式です。お問い合わせは
+            <a href="https://github.com/nanase/kemov/issues" target="_blank">issue</a> までご連絡ください。
+          </li>
+        </ul>
+      </div>
+    </template>
+  </InfiniteLoading>
+</template>
+
+<style lang="scss">
+@use '@/style/media';
+
+.streaming-list {
+  width: 85%;
+  margin: 20px auto;
+  padding: 15px 35px;
+  border-radius: 10px;
+  background-color: white;
+
+  @include media.size(lg) {
+    width: 815px;
+  }
+
+  @include media.size(md) {
+    width: 85%;
+  }
+
+  @include media.size(sm) {
+    width: 100%;
+    padding: 15px 0;
+  }
+
+  .streaming-item {
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 0.3s ease;
+    }
+
+    &-enter-from,
+    &-leave-to {
+      opacity: 0;
+    }
+  }
+}
+</style>
--- src/genet/music/MusicApp.vue
+++ src/genet/music/MusicApp.vue
@@ -1,51 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import InfiniteLoading from 'v3-infinite-loading';
-import type { StateHandler } from 'v3-infinite-loading/lib/types';
-import { load as loadYaml } from 'js-yaml';
-import { StreamingSearch } from './search';
 
-import type { Streaming } from '@/types/genet';
-import StreamingItem from '@/components/genet/StreamingItem.vue';
+import StreamingList from '@/components/genet/StreamingList.vue';
 
-import streamingYaml from './streaming.yml?raw';
-
-const rawStreamings = loadYaml(streamingYaml) as Streaming[];
-
-const streamings = ref<Streaming[]>([...rawStreamings]);
-const displayedStreamings = ref<Streaming[]>([]);
-const ItemsPerLoaded = 10;
-const infiniteId = ref<number>(0);
-const finishScore = ref<number>(0);
-const streamingSearch = new StreamingSearch();
-
-const load = async (state: StateHandler) => {
-  if (displayedStreamings.value.length >= streamings.value.length) {
-    finishScore.value = Math.floor(Math.random() * 5);
-    state.complete();
-  } else {
-    const newItems = streamings.value.slice(
-      displayedStreamings.value.length,
-      displayedStreamings.value.length + ItemsPerLoaded,
-    );
-    displayedStreamings.value.push(...newItems);
-    state.loading();
-  }
-};
+const filterQuery = ref<string>('');
+const totalStreamingCount = ref<number>(0);
+const matchedStreamingCount = ref<number>(0);
 
 function updateSearchQuery(e: Event) {
   if (!(e.target instanceof HTMLInputElement)) return;
 
-  const query = e.target.value;
+  filterQuery.value = e.target.value;
+}
 
-  if (query.length === 0) {
-    streamings.value = [...rawStreamings];
-  } else {
-    streamings.value = streamingSearch.search(rawStreamings, query);
-  }
-
-  displayedStreamings.value = [];
-  infiniteId.value = infiniteId.value + 1; // force-reset the InfiniteLoading
+function onUpdateFilter(total: number, matched: number) {
+  totalStreamingCount.value = total;
+  matchedStreamingCount.value = matched;
 }
 </script>
 
@@ -62,60 +32,19 @@ function updateSearchQuery(e: Event) {
           @change="updateSearchQuery"
         />
       </div>
-      <div class="result-count" v-show="streamings != null">
-        <span v-if="streamings.length === rawStreamings.length">
-          {{ `${rawStreamings.length} 件の配信` }}
+      <div class="result-count" v-show="filterQuery != null">
+        <span v-if="matchedStreamingCount === totalStreamingCount">
+          {{ `${totalStreamingCount} 件の配信` }}
         </span>
         <span v-else>
-          {{ `${rawStreamings.length} 件の配信から ${streamings.length} 件 見つかりました` }}
+          {{ `${totalStreamingCount} 件の配信から ${matchedStreamingCount} 件 見つかりました` }}
         </span>
       </div>
     </div>
-    <div class="content">
-      <div class="streaming-list">
-        <TransitionGroup name="streaming-item">
-          <StreamingItem v-for="streaming in displayedStreamings" :key="streaming.video.id" :data="streaming" />
-        </TransitionGroup>
-      </div>
-      <InfiniteLoading :identifier="infiniteId" @infinite="load">
-        <template #complete>
-          <div
-            class="finish-score"
-            style="background-image: url('/kemov/genet/score0.svg')"
-            v-if="finishScore === 0"
-          ></div>
-          <div
-            class="finish-score"
-            style="background-image: url('/kemov/genet/score1.svg')"
-            v-if="finishScore === 1"
-          ></div>
-          <div
-            class="finish-score"
-            style="background-image: url('/kemov/genet/score2.svg')"
-            v-if="finishScore === 2"
-          ></div>
-          <div
-            class="finish-score"
-            style="background-image: url('/kemov/genet/score3.svg')"
-            v-if="finishScore === 3"
-          ></div>
-          <div
-            class="finish-score"
-            style="background-image: url('/kemov/genet/score4.svg')"
-            v-if="finishScore === 4"
-          ></div>
-          <div class="horizon"></div>
-          <div class="note">
-            <ul>
-              <li>
-                このサイトは非公式です。お問い合わせは
-                <a href="https://github.com/nanase/kemov/issues" target="_blank">issue</a> までご連絡ください。
-              </li>
-            </ul>
-          </div>
-        </template>
-      </InfiniteLoading>
-    </div>
+    <Suspense>
+      <StreamingList :filterQuery="filterQuery" @updateFilter="onUpdateFilter"></StreamingList>
+      <template #fallback>読み込んでいます...</template>
+    </Suspense>
   </div>
 </template>
 
@@ -123,24 +52,12 @@ function updateSearchQuery(e: Event) {
 @use '@/style/media';
 
 .music-app {
-  .streaming-item {
-    &-enter-active,
-    &-leave-active {
-      transition: opacity 0.3s ease;
-    }
-
-    &-enter-from,
-    &-leave-to {
-      opacity: 0;
-    }
-  }
-
-  .content,
   .search-box {
     width: 85%;
     margin: 20px auto;
     padding: 15px 35px;
     border-radius: 10px;
+    text-align: center;
 
     @include media.size(lg) {
       width: 815px;
@@ -154,14 +71,6 @@ function updateSearchQuery(e: Event) {
       width: 100%;
       padding: 15px 0;
     }
-  }
-
-  .content {
-    background-color: white;
-  }
-
-  .search-box {
-    text-align: center;
 
     .input {
       padding: 10px;
