9b500f318c89ff2f4f0c00c073cbafe0fe5b6105
Nanase <nanase@users.noreply.github.com>
2023-08-27 18:59:15 +0900

    Add env file

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GENET_MUSIC_LIST_URL: string;
+  readonly VITE_GENET_MUSIC_LIST_SUB_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
