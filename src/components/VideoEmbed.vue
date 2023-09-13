62b3c1c631a1aaec03617fbc933aff1d8a1251e3
Nanase <nanase@users.noreply.github.com>
2023-09-13 22:06:41 +0900

    Simplify DOM in VideoEmbed

--- src/components/VideoEmbed.vue
+++ src/components/VideoEmbed.vue
@@ -14,27 +14,23 @@ const start = computed(() => (Number.isFinite(video.position) ? `start=${video.p
 </script>
 
 <template>
-  <div class="player-container">
-    <iframe
-      :src="`${getEmbedURL(video.id)}?${start}`"
-      frameborder="0"
-      allowfullscreen
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    >
-    </iframe>
-  </div>
+  <iframe
+    class="video-embed"
+    :src="`${getEmbedURL(video.id)}?${start}`"
+    frameborder="0"
+    allowfullscreen
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  >
+  </iframe>
 </template>
 
 <style lang="scss">
-.player-container {
+.video-embed {
+  display: block;
+  width: 100%;
+  height: 100%;
   min-width: 160px;
   aspect-ratio: 16 / 9;
   background: #000;
-
-  iframe {
-    width: 100%;
-    height: 100%;
-    display: block;
-  }
 }
 </style>
