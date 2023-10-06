267949b911a6a300fc4466cfb4af93bd264949a9
Nanase <nanase@users.noreply.github.com>
2023-10-06 14:07:29 +0900

    Fix correct channelsUri in realtime stats

--- src/stats/config.ts
+++ src/stats/config.ts
@@ -1,2 +1,2 @@
-export const channelsUri = 'https://raw.githubusercontent.com/nanase/asset/main/kemov/channels.json';
+export const channelsUri = 'https://nanase.cc/asset/kemov/channels.json';
 export const statsUri = 'https://s3.ap-northeast-1.amazonaws.com/nanase.asset/kemov/stats.json';
