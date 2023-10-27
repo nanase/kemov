a17297a6f33f157cd4250454ac47e6972f9fa553
Nanase <nanase@users.noreply.github.com>
2023-10-27 18:18:24 +0900

    `withCommas` function allows undefined params

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -1,4 +1,8 @@
 // from: https://stackoverflow.com/a/2901298
-export function withCommas(x: number): string {
+export function withCommas(x?: number): string {
+  if (x == null) {
+    return '';
+  }
+
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
