a8863d07a2e5da39ae6162f8df07cd97ffca8f97
Nanase <nanase@users.noreply.github.com>
2024-02-16 10:32:49 +0900

    Use definePeriodicCall in UpdateTime.vue

--- src/components/stats/UpdateTime.vue
+++ src/components/stats/UpdateTime.vue
@@ -1,40 +1,29 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref, watch } from 'vue';
+import { ref, watch } from 'vue';
 
+import { definePeriodicCall } from '@/lib/vue';
 import dayjs, { Dayjs } from '@/lib/dayjs';
+import { computed } from 'vue';
 
 const { time } = defineProps<{
   time: Dayjs;
 }>();
 
 const elapsedTime = ref<number>(Number.NaN);
-const elapsedTimeUpdateInterval = ref<number>();
+const readableElapsedTime = computed(() =>
+  elapsedTime.value >= 60 ? `${Math.floor(elapsedTime.value / 60)}分` : `${elapsedTime.value}秒`,
+);
 
-function readableElapsedTime() {
-  if (elapsedTime.value >= 60) {
-    return `${Math.floor(elapsedTime.value / 60)}分`;
-  } else {
-    return `${elapsedTime.value}秒`;
-  }
-}
-
-function updateElapsedTime() {
+async function updateElapsedTime() {
   elapsedTime.value = dayjs().diff(time, 's');
+  return 1;
 }
-
-onMounted(async () => {
-  elapsedTimeUpdateInterval.value = setInterval(updateElapsedTime, 1000);
-});
-
-onBeforeUnmount(() => {
-  clearInterval(elapsedTimeUpdateInterval.value);
-});
-
+definePeriodicCall(updateElapsedTime);
 watch(() => time, updateElapsedTime);
 </script>
 
 <template>
   <div class="update-time" v-if="time.isValid() && !Number.isNaN(elapsedTime)">
-    {{ `${time.format('YYYY/MM/DD HH:mm:ss')} (${readableElapsedTime()}前) 更新` }}
+    {{ `${time.format('YYYY/MM/DD HH:mm:ss')} (${readableElapsedTime}前) 更新` }}
   </div>
 </template>
