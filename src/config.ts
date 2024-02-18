8b3d4b6f02909ab7404d01d38896a7e842bf4453
Nanase <nanase@users.noreply.github.com>
2024-02-19 08:28:53 +0900

    Update json URIs to new server

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const channelsUri = 'https://nanase.cc/asset/kemov/channels.json';
-export const statsUri = 'https://s3.ap-northeast-1.amazonaws.com/nanase.asset/kemov/stats.json';
-export const videoUriBase = 'https://s3.ap-northeast-1.amazonaws.com/nanase.asset/kemov/stats/video/';
+export const statsUri = 'https://d1zvseiqyto6c5.cloudfront.net/kemov/stats.json';
+export const videoUriBase = 'https://d1zvseiqyto6c5.cloudfront.net/kemov/stats/video/';
 
 export function videoUri(channelId: string | undefined) {
   return `${videoUriBase}${channelId}.json`;
