039f9ccd6c411b48ad964abb1814603b8a620c01
Nanase <nanase@users.noreply.github.com>
2024-09-25 15:06:07 +0900

    Add colors in theme.ts

--- src/stats/theme.ts
+++ src/stats/theme.ts
@@ -9,6 +9,18 @@ const light: ThemeDefinition = {
     secondary: colors.indigo.base,
     positive: colors.green.darken3,
     negative: colors.red.darken3,
+    //
+    v2DrawerBackground: '#ffffff',
+    v2DrawerList: '#3c3c43',
+    v2DrawerListActive: colors.indigo.base,
+    v2AppBarBackground: '#ffffff',
+    v2SnackbarBackground: '#171717',
+  },
+
+  variables: {
+    'high-emphasis-opacity': '1.0',
+    'v2-content-color': '#3c3c43',
+    'v2-snackbar-border': 'none',
   },
 };
 
@@ -20,6 +32,17 @@ const dark: ThemeDefinition = {
     secondary: colors.indigo.darken4,
     positive: colors.green.lighten2,
     negative: colors.red.lighten1,
+    //
+    v2DrawerBackground: '#171717',
+    v2DrawerList: '#bcbcbc',
+    v2DrawerListActive: colors.blue.lighten2,
+    v2AppBarBackground: '#171717',
+    v2SnackbarBackground: '#171717',
+  },
+  variables: {
+    'high-emphasis-opacity': '0.86',
+    'v2-content-color': 'rgba(255, 255, 245, .86)',
+    'v2-snackbar-border': '1px solid #3c3c43',
   },
 };
 
